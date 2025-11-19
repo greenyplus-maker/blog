7af9020070a02b41a1ef377801917f6f298cf8e5
greenyplus-maker <jinalee@jinaui-MacBookPro.local>
2025-11-19 15:46:49 +0900

    blog6

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="min-h-screen flex flex-col bg-white text-gray-900">
+      <body className="min-h-screen flex flex-col bg-white text-gray-900 mt-[100px]">
         <Header />
         <main className="flex-grow w-full">
           {children}
